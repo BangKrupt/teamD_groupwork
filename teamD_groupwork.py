bd098f10b288d0b7db0740b9fb73e891447db548
isuh88 <isuh88@snu.ac.kr>
2022-05-24 20:55:38 +0900

    data+algorithm base

--- teamD_groupwork.py
+++ teamD_groupwork.py
@@ -1,7 +1,19 @@
-n = input("2~6 사이 정수 입력: ")
+import random, pandas
 
-array = [[1, 8, 7], [4, 3, 9], [5, 2, 6]]
+n = int(input("2~6사이 정수를 입력하세요:"))
 
+# Part 1(데이터 생성): 사용자가 n의 값을 입력하면, n by n matrix 생성하고 1~100 사이 무작위 정수로 구성함.
+def CreateRandomData(n):
+    data = {}
+    for i in range(1, n + 1):
+        data["작업{}".format(i)] = [random.randrange(1, 101) for i in range(1, n + 1)]
+    df = pandas.DataFrame(data)
+    df.index = ['기계{}'.format(i) for i in range(1, n + 1)]
+    print(df)
+    global array
+    array = [list(df.iloc[j]) for j in range(n)]
+
+# Part 2
 def sub_min_row(size):
     min_row = [min(row) for row in array]
     for i in range(size):
@@ -10,13 +22,12 @@ def sub_min_row(size):
 
 def sub_min_col(size):
     min_col = [min(col) for col in zip(*array)]
-
     for i in range(size):
         for j in range(size):
             array[j][i] -= min_col[i]
 
 if __name__ == '__main__':
-
-    sub_min_row(3)
-    sub_min_col(3)
+    CreateRandomData(n)
+    sub_min_row(n)
+    sub_min_col(n)
     print(array)
