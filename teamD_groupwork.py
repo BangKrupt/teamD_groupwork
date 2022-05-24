ec16063781ea39ca54fec3b1791231e5d624f31c
SeoHyunLee29 <102493613+SeoHyunLee29@users.noreply.github.com>
2022-05-24 20:28:00 +0900

    Update teamD_groupwork.py

--- teamD_groupwork.py
+++ teamD_groupwork.py
@@ -1,7 +1,6 @@
 import random,pandas
 
 n=int(input("2~6사이 정수를 입력하세요:"))
-CreateRandomData(n)
 
 # Part 1(데이터 생성): 사용자가 n의 값을 입력하면, n by n matrix 생성하고 1~100 사이 무작위 정수로 구성함.
 def CreateRandomData(n):
@@ -11,6 +10,8 @@ def CreateRandomData(n):
     df = pandas.DataFrame(data)
     df.index=['기계{}'.format(i) for i in range(1,n+1)]
     print(df)
+    
+CreateRandomData(n)
 
 
 
