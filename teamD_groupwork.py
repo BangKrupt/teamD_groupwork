78dab0bdd16ee5ac46e085c40e760913c67fa075
isuh88 <isuh88@snu.ac.kr>
2022-05-24 19:04:59 +0900

    algorithm_base

--- teamD_groupwork.py
+++ teamD_groupwork.py
@@ -1,1 +1,22 @@
 n = input("2~6 사이 정수 입력: ")
+
+array = [[3, 8, 9], [4, 12, 7], [4, 8, 5]]
+
+def sub_min_row(size):
+    min_row = [min(row) for row in array]
+    for i in range(size):
+        for j in range(size):
+            array[i][j] -= min_row[i]
+
+def sub_min_col(size):
+    min_col = [min(col) for col in zip(*array)]
+
+    for i in range(size):
+        for j in range(size):
+            array[j][i] -= min_col[i]
+
+if __name__ == '__main__':
+
+    sub_min_row(3)
+    sub_min_col(3)
+    print(array)
